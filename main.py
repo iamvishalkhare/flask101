6fb877d5bdaacdc10b8f7072c2c60db7d9753699
Vishal <vishal@1MG-LPTP-1341.local>
2021-10-05 01:24:35 +0530

    Making change 1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def main():
-    message = "Hello world"
+    message = "Hello world1"
     return message
     
 if __name__ == "__main__":
